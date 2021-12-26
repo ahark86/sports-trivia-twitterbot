4784d5f0be6e524bbf7c385d53355613fd6d2da9
Alex Harker <alexnharker@gmail.com>
2021-12-26 12:09:00 -0500

    adds more fault tolerance

--- main.py
+++ main.py
@@ -27,29 +27,34 @@ def answer_tweet(q, a):
 
 if os.path.exists('answer_file.txt'):
     # tweet answer from the file and then delete the file
-    with open('answer_file.txt') as f:
-        ans_tweet = f.readlines()
-        status_update = f'{ans_tweet[0].strip()}\n\n' \
-                        f'{ans_tweet[2].strip()}'
+    try:
+        with open('answer_file.txt') as f:
+            ans_tweet = f.readlines()
+            status_update = f'{ans_tweet[0].strip()}\n\n' \
+                            f'{ans_tweet[2].strip()}'
 
-    twitter = Twython(APIKEY, APISECRETKEY, ACCESSTOKEN, ACCESSTOKENSECRET)
-    twitter.update_status(status=status_update)
-    os.remove('answer_file.txt')
+        twitter = Twython(APIKEY, APISECRETKEY, ACCESSTOKEN, ACCESSTOKENSECRET)
+        twitter.update_status(status=status_update)
+        os.remove('answer_file.txt')
+    except:
+        os.remove('answer_file.txt')
 else:
     # pull question/answers, generate question and answer tweet, tweet question, create file to hold answer
     URL = "https://opentdb.com/api.php?amount=1&category=21&type=multiple"
+    try:
+        r = requests.get(URL).json()
+        question = html.unescape(r["results"][0]["question"])
+        cor_answer = html.unescape(r["results"][0]["correct_answer"])
+        inc_answer_1 = html.unescape(r["results"][0]["incorrect_answers"][0])
+        inc_answer_2 = html.unescape(r["results"][0]["incorrect_answers"][1])
+        inc_answer_3 = html.unescape(r["results"][0]["incorrect_answers"][2])
 
-    r = requests.get(URL).json()
-    question = html.unescape(r["results"][0]["question"])
-    cor_answer = html.unescape(r["results"][0]["correct_answer"])
-    inc_answer_1 = html.unescape(r["results"][0]["incorrect_answers"][0])
-    inc_answer_2 = html.unescape(r["results"][0]["incorrect_answers"][1])
-    inc_answer_3 = html.unescape(r["results"][0]["incorrect_answers"][2])
+        status_update = question_tweet(question, cor_answer, inc_answer_1, inc_answer_2, inc_answer_3)
 
-    status_update = question_tweet(question, cor_answer, inc_answer_1, inc_answer_2, inc_answer_3)
+        twitter = Twython(APIKEY, APISECRETKEY, ACCESSTOKEN, ACCESSTOKENSECRET)
+        twitter.update_status(status=status_update)
 
-    twitter = Twython(APIKEY, APISECRETKEY, ACCESSTOKEN, ACCESSTOKENSECRET)
-    twitter.update_status(status=status_update)
-
-    with open('answer_file.txt', 'w') as f:
-        f.write(answer_tweet(question, cor_answer))
+        with open('answer_file.txt', 'w') as f:
+            f.write(answer_tweet(question, cor_answer))
+    except:
+        pass
